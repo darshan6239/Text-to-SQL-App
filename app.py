43b994f1f1479b7d25e996aba6b0b2bc4c9dfdcb
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-07-14 22:07:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from langchain_core.output_parsers import StrOutputParser
 # --------------------------------------------------
 # PAGE CONFIG
 # --------------------------------------------------
-
 st.set_page_config(
     page_title="AI CSV Analyst",
     page_icon="📊",
